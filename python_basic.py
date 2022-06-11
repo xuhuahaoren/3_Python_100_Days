61d5e237c41f72812f392b5a1e4a03c319f06ac3
zjdxh <zjdxh8647@hotmail.com>
2022-06-11 22:59:39 +0800

    add

--- python_basic.py
+++ python_basic.py
@@ -107,7 +107,7 @@ print(float(10086))
 print(int('10086'))
 print(complex(10086))
 
-#----------2 Python数据类型
+#----------2 Python数据类型----------------------------------------------------------------------------------------------
 # 列表（list），元组（tuple），集合（set），字典（dict）
 #----------2.1 列表（list）
 # 用来存储一连串元素的容器，列表用[]来表示，其中元素的类型可不相同。
@@ -116,4 +116,67 @@ print(mylist)
 
 # 列表索引和切片
 # 索引从0开始，含左不含右
-print('[4]=', mylist[4])
+print('[4]=', mylist[4])  # 4
+print('[-4]=', mylist[-4])  # 2
+print('[0:4]=', mylist[0:4])  # [0, 1, 2, 3]
+print('[:4]=', mylist[:4])  # [0, 1, 2, 3]
+print('[4:]=', mylist[4:])  # [4, 5]
+print('[0:4:2]=', mylist[0:4:2])  # [0, 2]
+print('[-5:-1:]=', mylist[-5:-1:])  # [1, 2, 3, 4]
+print('[-2::-1]=', mylist[-2::-1])  # [4, 3, 2, 1, 1]
+
+# 修改列表
+mylist[3] = "小月"
+print(mylist[3])
+mylist[5] = "晓楠"
+print(mylist[5])
+mylist[5] = 19787
+print(mylist[5])
+print(mylist)
+
+# 插入元素
+mylist.append('han')
+mylist.extend(['long', 'wan'])  # 用于在列表末尾一次性追加另一个序列中的多个值（用新列表扩展原来的列表）
+print(mylist)
+
+scores = [90, 80, 75, 66]
+mylist.insert(1, scores)  # 添加到指定位置
+print(mylist)
+
+# 删除元素
+print(mylist.pop(1))  # 该函数返回被弹出的元素，不传入参数则删除最后一个元素
+print(mylist)
+
+# 判断元素是否在列表中
+print('wan' in mylist)
+print('han' not in mylist)
+
+print(mylist.count('wan'))
+print(mylist.index('wan'))
+
+# range函数生成整数列表
+print(range(10))
+print(range(-5, 5))
+print(range(-10, 10, 2))
+print(range(16, 10, -1))
+
+
+#----------2.2 元组（tuple）
+# 元组类似列表，元组里面的元素也是进行索引计算。列表里面的元素的值可以修改，而元组里面的元素的值不能修改，只能读取。
+# 元组的符号是()。
+studentsTuple = ("ming", "jun", "qiang", "wu", scores)
+print(studentsTuple)
+
+try:
+    studentsTuple[1] = 'fu'
+except TypeError:
+    print('TypeError')
+
+scores[1] = 100  # 元组内的列表可以更改元素数值
+print(studentsTuple)
+
+print('ming' in studentsTuple)  # 元组支持判断元组内元素是否存在
+print(studentsTuple[:4])
+print(studentsTuple.count('ming'))
+print(studentsTuple.index('jun'))
+print(len(studentsTuple))
